ed0a406f33e2ed0bf21bd29296bc259715fb7bed
Yu V <valeriyyugay96@yandex.ru>
2023-03-05 19:09:34 +0300

    Code Changes

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,10 @@
 public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 20000;
+        int bonus = 20;
+        int mile = ticketPrice / bonus;
+
+        System.out.println("Итоговый бонус:" + mile);
+        
+    }
 }
